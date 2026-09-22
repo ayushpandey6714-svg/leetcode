5dd7a19a2c2734fc8ee24d4d2bf470e494edd300
ayushpandey6714-svg <ayushpandey6714@gmail.com>
2026-09-22 17:12:05 +0530

    Time: 514 ms (11.38%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT e.name  
+FROM Employee e  
+JOIN  
+    (  
+        SELECT managerId  
+        FROM Employee  
+        GROUP BY managerId  
+        HAVING COUNT(*) >= 5  
+    ) temp  
+    ON e.id = temp.managerId;  
